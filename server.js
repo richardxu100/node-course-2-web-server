a4390186c1fdbafe31f97449ee7c42388756aeb2
Rich Xu <richardxu100@gmail.com>
2016-10-28 18:07:09 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials'); // absolute path
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  // fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next(); // need this, or nothing loads
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs'); // next never called, so app.get() isn't called
+});
+
+// this is middleware, app.use()
+app.use(express.static(__dirname + '/public')); // in the wrapper function, __dirname
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to the home page!'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'About Page',
+    welcomeMessage: 'Welcome to the about page!'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad request man!'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('The server is up on port 3000');
+});
